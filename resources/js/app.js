33603e9df4616fe2b7eea11c2a38a2f33f3c8f4b
norbus2000 <odornorbert.on@gmail.com>
2022-12-04 22:32:52 +0100

    bootstrap fix

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,8 @@
  * First we will load all of this project's JavaScript dependencies which
  * includes React and other helpers. It's a great starting point while
  * building robust, powerful web applications using React + Laravel.
- import "./bootstrap";
  */
+import './bootstrap';
 
 /**
  * Next, we will create a fresh React component instance and attach it to
@@ -11,5 +11,5 @@
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-import "@mui/material";
-import "./ReactApp";
+import '@mui/material';
+import './ReactApp';
